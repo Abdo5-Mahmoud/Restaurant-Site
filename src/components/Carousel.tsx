c262cc9130a12f1841bf15f8e70b3769c6f3015e
Abdo5-Mahmoud <abdofwzy9@gmali.com>
2025-11-08 20:45:47 +0200

    update carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,16 +3,16 @@ import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
   superLargeDesktop: {
-    breakpoint: { max: 2560, min: 0 },
+    breakpoint: { max: 4000, min: 3000 },
     items: 2,
   },
   desktop: {
-    breakpoint: { max: 1024, min: 0 },
+    breakpoint: { max: 3000, min: 1024 },
     items: 2,
   },
   tablet: {
-    breakpoint: { max: 768, min: 0 },
-    items: 1,
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
